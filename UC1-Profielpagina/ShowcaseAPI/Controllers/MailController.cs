cee15dcc6cbab336b3399fb0f7abb8a4d35de078
Roey Langbroek <roeygaming07@gmail.com>
2025-02-24 10:23:46 +0100

    Merged

---
+++ UC1-Profielpagina/ShowcaseAPI/Controllers/MailController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShowcaseAPI.Models;
+using System.Net.Mail;
+using System.Net;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ShowcaseAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailController : ControllerBase
+    {
+        // POST api/<MailController>
+        [HttpPost]
+        public ActionResult Post([Bind("FirstName, LastName, Email, Phone, Subject, Message")] Contactform form)
+        {
+            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("81ea129c34384a", "273021a495869a"),
+                EnableSsl = true
+            };
+
+            var toAddress = new MailAddress("s1204257@student.windesheim.nl", "Roey Langbroek");
+            var fromAddress = new MailAddress(form.Email, $"{form.FirstName} {form.LastName}");
+            var mailMessage = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = form.Subject,
+                Body = $"{form.Message} \nTelefoonnummer: {form.Phone}",
+                IsBodyHtml = false
+            };
+
+            client.Send(mailMessage);
+            System.Console.WriteLine("Sent");
+
+            return Ok();
+        }
+    }
+}
---
+++ UC2-Contactpagina/Showcase-Contactpagina/wwwroot/js/gdpr.js
@@ -0,0 +1,70 @@
+class GDPR {
+
+    constructor() {
+        this.showStatus();
+        this.showContent();
+        this.bindEvents();
+
+        if (this.cookieStatus() !== 'accept') this.showGDPR();
+    }
+
+    bindEvents() {
+        let buttonAccept = document.querySelector('.gdpr-consent__button--accept');
+        let buttonReject = document.querySelector('.gdpr-consent__button--reject');
+
+        buttonAccept.addEventListener('click', () => {
+            this.cookieStatus('accept');
+            this.showStatus();
+            this.showContent();
+            this.hideGDPR();
+        });
+
+        buttonReject.addEventListener('click', () => {
+            this.cookieStatus('reject');
+            this.showStatus();
+            this.showContent();
+            this.hideGDPR();
+        });
+    }
+
+    showContent() {
+        this.resetContent();
+        const status = this.cookieStatus() == null ? 'not-chosen' : this.cookieStatus();
+        const element = document.querySelector(`.content-gdpr-${status}`);
+        element.classList.add('show');
+    }
+
+    resetContent() {
+        const classes = [
+            '.content-gdpr-accept',
+            '.content-gdpr-reject',
+            '.content-gdpr-not-chosen'
+        ];
+
+        for (const c of classes) {
+            document.querySelector(c).classList.add('hide');
+            document.querySelector(c).classList.remove('show');
+        }
+    }
+
+    showStatus() {
+        document.getElementById('content-gpdr-consent-status').innerHTML =
+            this.cookieStatus() == null ? 'Niet gekozen' : this.cookieStatus();
+    }
+
+    cookieStatus(status) {
+        if (status) localStorage.setItem('gdpr-consent-choice', status);
+        return localStorage.getItem('gdpr-consent-choice');
+    }
+
+    hideGDPR() {
+        document.querySelector(`.gdpr-consent`).classList.add('hide');
+        document.querySelector(`.gdpr-consent`).classList.remove('show');
+    }
+
+    showGDPR() {
+        document.querySelector(`.gdpr-consent`).classList.add('show');
+    }
+}
+
+const gdpr = new GDPR();
